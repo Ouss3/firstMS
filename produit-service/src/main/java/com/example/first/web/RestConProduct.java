9188902505a9138d2da7d32cf09690cfd1a5f0d5
oussama <oukhayloussama@gmail.com>
2023-11-29 16:38:21 +0100

    add gateway

--- produit-service/src/main/java/com/example/first/web/RestConProduct.java
+++ produit-service/src/main/java/com/example/first/web/RestConProduct.java
@@ -1,7 +1,27 @@
 package com.example.first.web;
 
+import com.example.first.entites.Product;
+import com.example.first.reposotry.ProductRepositry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RestConProduct {
+
+    @Autowired
+    ProductRepositry productRepositry;
+    @GetMapping("/products")
+    public List<Product> getAll(){
+        return productRepositry.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product productbyid(@PathVariable Integer id){
+        return productRepositry.findById(id).orElseThrow(()-> new RuntimeException("Product not found"));
+    }
+
 }
